bd6d4ef07463266850206ea489b5efc7b7d32d5c
Luciano Albis <lucianoalbis@yahoo.com.br>
2017-11-20 20:03:38 -0200

    Atualizando o Deploy
    Automação

--- deploy.php
+++ deploy.php
@@ -1,1 +1,29 @@
 
+<?php
+
+	//$myfile = fopen("logCommitsBitBucket.txt", "w");
+	//fwrite($myfile, "Webhook me acionou");
+	//fclose($myfile);
+
+	if(function_exists('exec')) {
+    	echo "exec is enabled\n";
+
+    	exec("sudo git clean -f -d 2>&1", $output, $return_val);
+    	if($return_val !== 0) {
+		    echo "Error 1\n";
+		    print_r($output);   
+			}	
+
+    	exec("sudo git pull -f 2>&1", $output, $return_val);
+		if($return_val !== 0) {
+		    echo "Error 2\n";
+		    print_r($output);
+		}
+		
+    	echo "\nArquivos atualizados";
+	}
+	else {
+		echo "\nexec is disabled";
+	}
+
+?>
