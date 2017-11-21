6c36e6cdb41510b12e9515d558ab7070b0890dc6
Luciano Albis <lucianoalbis@yahoo.com.br>
2017-11-20 22:10:45 -0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -31,20 +31,12 @@
 	if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
 	  //exec( 'cd /var/www/html/teste-bertholdo-github/ && git reset --hard HEAD && git pull' );
 		
-			$myfile = fopen("logCommitsBitBucket.txt", "w");
-			
-					
-		exec("git pull -f 2>&1", $output, $return_val);
-		if($return_val !== 0) {
-		    //echo "Error 2\n";
-		    //print_r($output);
-			
-			fwrite($myfile, "no");
-		}
-		else
-		{
-			fwrite($myfile, "ok");
-		}
+		$myfile = fopen("logCommitsBitBucket.txt", "w");	
+		$whoami = shell_exec("whoami");
+		fwrite($myfile, $whoami);
+		
+		$git_pull = shell_exec("git pull 2>&1");
+		fwrite($myfile, $git_pull);
 		
 		fclose($myfile);
 	}
