0b98bfc408132e01e7034433518a6c4d7e1b5086
Luciano Albis <lucianoalbis@yahoo.com.br>
2017-11-22 16:30:01 -0200

    Mudando arquivo sh

--- pull.php
+++ pull.php
@@ -1,6 +1,3 @@
-php<?php
-    exec('cd /var/www/html/testebertholdogithub 2>&1' , $output, $return_var);
-    //print_r($output);
-    exec('git pull 2>&1', $output, $return_var);
-    //print_r($output);
+<?php
+    exec('git --work-tree=/var/www/html/testebertholdogithub --git-dir=/var/www/html/testebertholdogithub/.git pull origin master 2>&1' , $output, $return_var);
 ?>
